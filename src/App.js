35a53d4cf3560aac06879849bb09bbec28ebd027
elaitala <elaitala@gmail.com>
2020-03-11 09:40:01 -0700

    Adds NAV bar and DASHBOARD component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Dashboard from './components/Dashboard/Dashboard'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Builders Waste Recycling
+          </Navbar.Brand>
+        </Navbar>
+
+        <Dashboard />
+          
+        <h2>Job Dashboard:</h2>
+
+        <h2>Trailers in the field:</h2>
+        
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Dashboard.css';
+import Table from 'react-bootstrap/Table';
+
+class Dashboard extends React.Component {
+
+  render () {
+    return (
+      <>
+      <div>
+        <Table striped border hover responsive="sm" variant="dark">
+          <thead>
+            <tr>
+              <th>Trailer</th>
+              <th>Total</th>
+              <th>Out</th>
+              <th>Available</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>10 yd</td>
+              <td>150</td>
+              <td>140</td>
+              <td>10</td>
+            </tr>
+            <tr>
+              <td>15 yd</td>
+              <td>60</td>
+              <td>55</td>
+              <td>5</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>210</td>
+              <td>195</td>
+              <td>15</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+      </>
+    )  
+  }
+}
+
+export default Dashboard;
